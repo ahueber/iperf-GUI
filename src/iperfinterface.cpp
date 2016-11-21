b8f67109e144ecd328e5af2cac03118f82d72b4b
Toby Merz <realtiaz@gmail.com>
2016-11-21 10:43:08 +0100

    update: extended by regex parsing iperf status messages

--- src/iperfinterface.cpp
+++ src/iperfinterface.cpp
@@ -1,6 +1,53 @@
 #include "iperfinterface.h"
 
-IperfInterface::IperfInterface()
-{
+IperfInterface::IperfInterface(QString initialArguments) {
+    this->initialArguments = initialArguments;
 
+    this->setProcessChannelMode(QProcess::MergedChannels);
+    QObject::connect(this, SIGNAL(readyReadStandardOutput()), this, SLOT(processReadyReadStandardOutput()));
+}
+
+IperfInterface::~IperfInterface() {
+    this->terminate();
+}
+
+QStringList IperfInterface::parseArguments(QString arguments) {
+    QStringList parsedArguments;
+    for (QString argument: arguments.split("-", QString::SkipEmptyParts)) {
+        if (argument.at(argument.length() - 1) == QString(" "))
+            argument = argument.left(argument.length() - 1);
+        parsedArguments << QString("-").append(argument);
+    }
+    return parsedArguments;
+}
+
+QString IperfInterface::getInitialArguments() {
+    return this->initialArguments;
+}
+
+void IperfInterface::setInitialArguments(QString initialArguments) {
+    this->initialArguments = initialArguments;
+}
+
+void IperfInterface::run() {
+    QStringList parsedArguments = this->parseArguments(this->initialArguments);
+    this->start(IPERF_PATH_AND_FILENAME, parsedArguments);
+    this->waitForReadyRead();
+}
+
+void IperfInterface::processReadyReadStandardOutput() {
+    QString rawOutput = QString(this->readAllStandardOutput());
+
+    if (rawOutput.contains(QRegExp(MSG_SERVER_LISTENING))) {
+        this->serverIsListening = true;
+    }
+
+    if (rawOutput.contains(QRegExp(MSG_CONNECTION_ESTABLISHED))) {
+        qDebug() << "Connected!";
+    }
+
+    QStringList lines = rawOutput.split("\n");
+    foreach (QString line, lines) {
+        emit this->logOutput(line);
+    }
 }
--- src/iperfinterface.h
+++ src/iperfinterface.h
@@ -2,11 +2,43 @@
 #define IPERFINTERFACE_H
 
 #include <QObject>
+#include <QProcess>
+#include <QString>
+#include <QStringList>
+#include <QByteArray>
+#include <QRegExp>
+#include <QDebug>
 
-class IperfInterface : public QProcess
-{
+#define IPERF_PATH_AND_FILENAME "/usr/bin/iperf"
+#define MSG_SERVER_LISTENING "Server listening on UDP port [0-9]+"
+#define MSG_CONNECTION_ESTABLISHED "[a-zA-Z0-9]+ (?:[0-9]{1,3}.){3}[0-9]{1,3} port [0-9]+ connected with (?:[0-9]{1,3}.){3}[0-9]{1,3} port [0-9]+"
+
+
+class IperfInterface : public QProcess {
+    Q_OBJECT
+
+private:
+    QString initialArguments;
+
+    QStringList parseArguments(QString arguments);
+
 public:
-    IperfInterface();
+    bool serverIsListening = false;
+
+    IperfInterface(QString initialArguments = QString());
+    ~IperfInterface();
+
+    void setInitialArguments(QString initialArguments);
+    QString getInitialArguments();
+
+    void run();
+
+signals:
+    void logOutput(const QString &);
+
+public slots:
+    void processReadyReadStandardOutput();
+
 };
 
 #endif // IPERFINTERFACE_H
